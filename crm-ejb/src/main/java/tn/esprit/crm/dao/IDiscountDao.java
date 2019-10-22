7deb84a96367460c984886d0c00aa670b45997f2
ahmedderbel <ahmed.derbel@esprit.tn>
2019-10-22 22:00:34 +0100

    discount

--- crm-ejb/src/main/java/tn/esprit/crm/dao/IDiscountDao.java
+++ crm-ejb/src/main/java/tn/esprit/crm/dao/IDiscountDao.java
@@ -3,8 +3,10 @@ package tn.esprit.crm.dao;
 import javax.ejb.Local;
 
 import tn.esprit.crm.entities.Discount;
+import tn.esprit.crm.entities.Product;
 
 @Local
 public interface IDiscountDao extends IGenericDao<Discount> {
 
+	//public boolean AffecterDiscountToProduct(Product p,int id_discount);
 }
