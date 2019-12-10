2b0b6129db46150c2fba3a04184020c8e2d368a7
Triki-Ahmed <ahmed.triki@esprit.tn>
2019-12-10 15:07:35 +0100

    jee fix

--- crm-web/src/main/java/tn/esprit/crm/resources/DocumentResource.java
+++ crm-web/src/main/java/tn/esprit/crm/resources/DocumentResource.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
+import javax.annotation.security.PermitAll;
 import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonObject;
